3cf3597eb2cf8ea43546a3820f468bea9465911a
jonathan-briere <jonathan.briere.dev@gmail.com>
2023-07-04 09:46:36 -0400

    Fixed Listing CSS

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -21,7 +21,7 @@ export const Listing = ({ locations }) => {
         <TableBody>
           {locations?.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.ip}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
